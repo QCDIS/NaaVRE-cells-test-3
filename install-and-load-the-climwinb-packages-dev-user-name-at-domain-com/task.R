54f2d918fb81e6c142bf47fc73a5516debf8a771
skoulouzis <skoulouzis@users.noreply.github.com>
2024-03-14 20:09:42 +0100

    install-and-load-the-climwinb-packages-dev-user-name-at-domain-com update

--- install-and-load-the-climwinb-packages-dev-user-name-at-domain-com/task.R
+++ install-and-load-the-climwinb-packages-dev-user-name-at-domain-com/task.R
@@ -55,6 +55,8 @@ temperature_zoo_str <- toString(temperature_zoo)
 rolling_mean_temp_str <- toString(rolling_mean_temp)
 temperature_data_str <- toString(temperature_data)
 
+a = 0.6542463187840682
+
 
 
 # capturing outputs
