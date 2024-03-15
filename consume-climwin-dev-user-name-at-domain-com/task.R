94673fe11e1883fa495fec9b1fdd82263275a1cc
skoulouzis <skoulouzis@users.noreply.github.com>
2024-03-15 10:30:00 +0100

    consume-climwin-dev-user-name-at-domain-com update

--- consume-climwin-dev-user-name-at-domain-com/task.R
+++ consume-climwin-dev-user-name-at-domain-com/task.R
@@ -25,7 +25,6 @@ make_option(c("--temperature_data_str"), action="store", default=NA, type="chara
 # set input parameters accordingly
 opt = parse_args(OptionParser(option_list=option_list))
 
-
 id <- gsub('"', '', opt$id)
 rolling_mean_temp_str <- gsub('"', '', opt$rolling_mean_temp_str)
 temperature_data_str <- gsub('"', '', opt$temperature_data_str)
@@ -37,7 +36,6 @@ temperature_data_str <- gsub('"', '', opt$temperature_data_str)
 
 cat("Original Temperature Data:\n", head(temperature_data_str), "\n\n")
 cat("Rolling Mean Temperature in Moving Windows:\n", head(coredata(rolling_mean_temp_str)), "\n")
-a = 0.22286614694881124
 
 
 
