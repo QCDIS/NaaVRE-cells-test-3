225907e4e190903b9a69f607a03b3ee2544e55f4
skoulouzis <skoulouzis@users.noreply.github.com>
2024-03-16 10:53:41 +0100

    loop-int-list-r-dev-user-name-at-domain-com update

--- loop-int-list-r-dev-user-name-at-domain-com/task.R
+++ loop-int-list-r-dev-user-name-at-domain-com/task.R
@@ -16,6 +16,7 @@ make_option(c("--list_of_ints"), action="store", default=NA, type="character", h
 # set input parameters accordingly
 opt = parse_args(OptionParser(option_list=option_list))
 
+
 id <- gsub('"', '', opt$id)
 list_of_ints = fromJSON(opt$list_of_ints)
 
