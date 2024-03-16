d3a54b35d644b256b287c9f3a211af7b36371b38
skoulouzis <skoulouzis@users.noreply.github.com>
2024-03-16 11:18:04 +0100

    install-and-load-the-climwinb-packages-dev-user-name-at-domain-com update

--- install-and-load-the-climwinb-packages-dev-user-name-at-domain-com/task.R
+++ install-and-load-the-climwinb-packages-dev-user-name-at-domain-com/task.R
@@ -23,7 +23,6 @@ make_option(c("--id"), action="store", default=NA, type="character", help="my de
 # set input parameters accordingly
 opt = parse_args(OptionParser(option_list=option_list))
 
-
 id <- gsub('"', '', opt$id)
 
 
@@ -54,8 +53,6 @@ rolling_mean_temp <- rollmean(temperature_zoo, k = window_size, fill = 0.0)
 temperature_zoo_str <- toString(temperature_zoo)
 rolling_mean_temp_str <- toString(rolling_mean_temp)
 temperature_data_str <- toString(temperature_data)
-
-a = 0.4387139529950047
 
 
 
