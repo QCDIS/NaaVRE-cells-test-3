88f46a39d66a07f66cc06dd87f9ac57179745aed
skoulouzis <skoulouzis@users.noreply.github.com>
2024-03-16 10:59:03 +0100

    loop-list-r-dev-user-name-at-domain-com update

--- loop-list-r-dev-user-name-at-domain-com/task.R
+++ loop-list-r-dev-user-name-at-domain-com/task.R
@@ -16,7 +16,6 @@ make_option(c("--list_of_paths"), action="store", default=NA, type="character", 
 # set input parameters accordingly
 opt = parse_args(OptionParser(option_list=option_list))
 
-
 id <- gsub('"', '', opt$id)
 list_of_paths = fromJSON(opt$list_of_paths)
 
