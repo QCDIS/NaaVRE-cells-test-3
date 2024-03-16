385623d228b73414cd8a836d83b3137c35f1c1d8
skoulouzis <skoulouzis@users.noreply.github.com>
2024-03-16 11:16:16 +0100

    loop-file-lines-r-dev-user-name-at-domain-com update

--- loop-file-lines-r-dev-user-name-at-domain-com/task.R
+++ loop-file-lines-r-dev-user-name-at-domain-com/task.R
@@ -16,6 +16,7 @@ make_option(c("--lines"), action="store", default=NA, type="character", help="my
 # set input parameters accordingly
 opt = parse_args(OptionParser(option_list=option_list))
 
+
 id <- gsub('"', '', opt$id)
 lines = fromJSON(opt$lines)
 
