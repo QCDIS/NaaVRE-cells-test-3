7ed97590c6cc52bc10d0dc7e555e0d3bdebb4b65
skoulouzis <skoulouzis@users.noreply.github.com>
2024-03-16 11:19:52 +0100

    add1-r-dev-user-name-at-domain-com update

--- add1-r-dev-user-name-at-domain-com/task.R
+++ add1-r-dev-user-name-at-domain-com/task.R
@@ -16,7 +16,6 @@ make_option(c("--id"), action="store", default=NA, type="character", help="my de
 # set input parameters accordingly
 opt = parse_args(OptionParser(option_list=option_list))
 
-
 count = opt$count
 id <- gsub('"', '', opt$id)
 
@@ -26,7 +25,6 @@ id <- gsub('"', '', opt$id)
 
 
 a = count + 1
-a = 0.7420639453846576
 
 
 
