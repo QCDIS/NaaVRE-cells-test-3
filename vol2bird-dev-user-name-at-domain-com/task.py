2c95a3e03e7f9e8592492f6bfed7c990c9b5d1fe
skoulouzis <skoulouzis@users.noreply.github.com>
2024-03-17 14:21:36 +0100

    vol2bird-dev-user-name-at-domain-com update

--- vol2bird-dev-user-name-at-domain-com/task.py
+++ vol2bird-dev-user-name-at-domain-com/task.py
@@ -23,8 +23,6 @@ cmd = "vol2bird --version"
 
 msg = os.system(cmd)  # returns the exit code in unix
 
-a = 0.22657997997138923
-
 import json
 filename = "/tmp/msg_" + id + ".json"
 file_msg = open(filename, "w")
