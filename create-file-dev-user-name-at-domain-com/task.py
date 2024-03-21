823e9076e062917afff7d64beefbc245e63af006
skoulouzis <skoulouzis@users.noreply.github.com>
2024-03-21 11:27:51 +0200

    create-file-dev-user-name-at-domain-com update

--- create-file-dev-user-name-at-domain-com/task.py
+++ create-file-dev-user-name-at-domain-com/task.py
@@ -31,6 +31,7 @@ fp.close()
 onlyfiles = [f for f in listdir(conf_data_folder) if isfile(join(conf_data_folder, f))]
 
 print(onlyfiles)
+a = 0.39824733534705514
 
 import json
 filename = "/tmp/file_path_" + id + ".json"
