4a552a847ed1e80552d739fce75a5b906286cc0a
skoulouzis <skoulouzis@users.noreply.github.com>
2024-04-09 09:24:29 +0200

    param-check-dev-user-name-domain-com update

--- param-check-dev-user-name-domain-com/task.py
+++ param-check-dev-user-name-domain-com/task.py
@@ -7,7 +7,8 @@ arg_parser.add_argument('--id', action='store', type=str, required=True, dest='i
 
 arg_parser.add_argument('--param_float', action='store', type=float, required=True, dest='param_float')
 arg_parser.add_argument('--param_int', action='store', type=int, required=True, dest='param_int')
-arg_parser.add_argument('--param_list', action='store', type=list, required=True, dest='param_list')
+arg_parser.add_argument('--param_list_int', action='store', type=list, required=True, dest='param_list_int')
+arg_parser.add_argument('--param_list_str', action='store', type=list, required=True, dest='param_list_str')
 arg_parser.add_argument('--param_string', action='store', type=str, required=True, dest='param_string')
 arg_parser.add_argument('--param_string_with_comment', action='store', type=str, required=True, dest='param_string_with_comment')
 
@@ -19,7 +20,8 @@ id = args.id
 
 param_float = args.param_float
 param_int = args.param_int
-param_list = args.param_list
+param_list_int = args.param_list_int
+param_list_str = args.param_list_str
 param_string = args.param_string
 param_string_with_comment = args.param_string_with_comment
 
@@ -28,5 +30,6 @@ print(param_string)
 print(param_string_with_comment)
 print(param_int)
 print(param_float)
-print(param_list)
+print(param_list_int)
+print(param_list_str)
 
