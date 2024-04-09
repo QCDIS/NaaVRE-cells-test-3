0c7b607be53c24205ef092b59ec6edc6ee322a8e
skoulouzis <skoulouzis@users.noreply.github.com>
2024-04-09 10:29:25 +0200

    param-check-dev-user-name-domain-com update

--- param-check-dev-user-name-domain-com/task.py
+++ param-check-dev-user-name-domain-com/task.py
@@ -7,8 +7,8 @@ arg_parser.add_argument('--id', action='store', type=str, required=True, dest='i
 
 arg_parser.add_argument('--param_float', action='store', type=float, required=True, dest='param_float')
 arg_parser.add_argument('--param_int', action='store', type=int, required=True, dest='param_int')
-arg_parser.add_argument('--param_list_int', action='store', type=list, required=True, dest='param_list_int')
-arg_parser.add_argument('--param_list_str', action='store', type=list, required=True, dest='param_list_str')
+arg_parser.add_argument('--param_list_int', action='store', type=str, required=True, dest='param_list_int')
+arg_parser.add_argument('--param_list_str', action='store', type=str, required=True, dest='param_list_str')
 arg_parser.add_argument('--param_string', action='store', type=str, required=True, dest='param_string')
 arg_parser.add_argument('--param_string_with_comment', action='store', type=str, required=True, dest='param_string_with_comment')
 
