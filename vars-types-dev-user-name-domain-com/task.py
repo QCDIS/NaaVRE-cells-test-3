3103d497fcd517b712b969472170bfc4cbc67e2e
skoulouzis <skoulouzis@users.noreply.github.com>
2024-04-11 15:42:04 +0200

    vars-types-dev-user-name-domain-com update

--- vars-types-dev-user-name-domain-com/task.py
+++ vars-types-dev-user-name-domain-com/task.py
@@ -26,7 +26,7 @@ conf_string_with_comment = 'param_string value'  # comment
 conf_int = 1
 conf_float = 1.1
 conf_list_int = [1, 2, 3]
-conf_list_str = ["1", "two", "3"]
+conf_list_str = ["1", " space in elem", "3"]
 
 var_string = 'var_string value'
 var_string_with_comment = 'var_string value'  # comment
