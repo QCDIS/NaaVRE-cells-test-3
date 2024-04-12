610c38440a11b38822c62534627719a867f56b7c
skoulouzis <skoulouzis@users.noreply.github.com>
2024-04-12 10:11:07 +0200

    input-lists-dev-user-name-at-domain-com update

--- input-lists-dev-user-name-at-domain-com/task.py
+++ input-lists-dev-user-name-at-domain-com/task.py
@@ -21,6 +21,7 @@ msg = args.msg.replace('"','')
 list_of_paths = ["/webdav/LAZ/targets_myname","/webdav/LAZ/targets_myname","/webdav/LAZ/targets_myname","/webdav/LAZ/targets_myname","/webdav/LAZ/targets_myname","/webdav/LAZ/targets_myname","/webdav/LAZ/targets_myname","/webdav/LAZ/targets_myname","/webdav/LAZ/targets_myname","/webdav/LAZ/targets_myname"]
 list_of_ints = [1,2,35,6,65]
 print(msg)
+a = 0.3368632710931643
 
 filename = "/tmp/list_of_paths_" + id + ".json"
 file_list_of_paths = open(filename, "w")
