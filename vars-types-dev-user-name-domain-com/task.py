2ad12f2bae517500d3c6b5684895a5e4ae2368ca
skoulouzis <skoulouzis@users.noreply.github.com>
2024-04-12 13:37:27 +0200

    vars-types-dev-user-name-domain-com update

--- vars-types-dev-user-name-domain-com/task.py
+++ vars-types-dev-user-name-domain-com/task.py
@@ -1,5 +1,6 @@
 
 import argparse
+import json
 arg_parser = argparse.ArgumentParser()
 
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
@@ -19,22 +20,21 @@ param_string_with_comment = 'param_string value'  # comment
 param_int = 1
 param_float = 1.1
 param_list_int = [1, 2, 3]
-param_list_str = ["1", "two", "3"]
+param_list_str = ["1", "space in elem", "3"]
 
 conf_string = 'param_string value'
 conf_string_with_comment = 'param_string value'  # comment
 conf_int = 1
 conf_float = 1.1
 conf_list_int = [1, 2, 3]
-conf_list_str = ["1", "two", "3"]
+conf_list_str = ["list_str", "space in elem", "3"]
 
 var_string = 'var_string value'
 var_string_with_comment = 'var_string value'  # comment
 var_int = 1
 var_float = 1.1
 var_list_int = [1, 2, 3]
-var_list_str = ["1", "two", "3"]
-a = 0.30604095653547503
+var_list_str = ["var_list_str", "space in elem", "3"]
 
 filename = "/tmp/var_string_" + id + ".json"
 file_var_string = open(filename, "w")
