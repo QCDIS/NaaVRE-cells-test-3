c9ce3749ee4109f841e635ca2fa7b25f5bf547cf
skoulouzis <skoulouzis@users.noreply.github.com>
2024-04-15 15:12:08 +0200

    assert-vars-dev-user-name-domain-com update

--- assert-vars-dev-user-name-domain-com/task.py
+++ assert-vars-dev-user-name-domain-com/task.py
@@ -49,17 +49,17 @@ except Exception as e:
 param_string = args.param_string.replace('"','')
 param_string_with_comment = args.param_string_with_comment.replace('"','')
 
-conf_float = 1.1
+conf_string = 'param_string value'
+
+conf_string_with_comment = 'param_string value'  # comment
 
 conf_int = 1
+
+conf_float = 1.1
 
 conf_list_int = [1, 2, 3]
 
 conf_list_str = ["list_str", "space in elem", "3"]
-
-conf_string = 'param_string value'
-
-conf_string_with_comment = 'param_string value'  # comment
 
 
 conf_string = 'param_string value'
@@ -93,5 +93,4 @@ assert param_list_int == check_list_int
 assert param_list_str == check_list_str
 
 print("All variables are the same.")
-a = 0.8486640798096086
 
