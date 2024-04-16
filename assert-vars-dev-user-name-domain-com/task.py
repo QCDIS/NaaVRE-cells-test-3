8842cf4938cabda2ceec53ebeea83770b4cf87dc
skoulouzis <skoulouzis@users.noreply.github.com>
2024-04-16 12:58:25 +0200

    assert-vars-dev-user-name-domain-com update

--- assert-vars-dev-user-name-domain-com/task.py
+++ assert-vars-dev-user-name-domain-com/task.py
@@ -31,7 +31,7 @@ try:
 except Exception as e:
     if e.__class__.__name__ == 'JSONDecodeError':
         import ast
-        param_list_int = ast.literal_eval(args.param_list_int.replace('[','["').replace(',','","').replace('" ','"').replace(']','"]')).replace("'","")
+        param_list_int = ast.literal_eval(args.param_list_int.replace('[','["').replace(',','","').replace('" ','"').replace(']','"]').replace("'",""))
     else:
         raise e
 print(args.param_list_str)
@@ -41,7 +41,7 @@ try:
 except Exception as e:
     if e.__class__.__name__ == 'JSONDecodeError':
         import ast
-        param_list_str = ast.literal_eval(args.param_list_str.replace('[','["').replace(',','","').replace('" ','"').replace(']','"]')).replace("'","")
+        param_list_str = ast.literal_eval(args.param_list_str.replace('[','["').replace(',','","').replace('" ','"').replace(']','"]').replace("'",""))
     else:
         raise e
 param_string = args.param_string.replace('"','')
