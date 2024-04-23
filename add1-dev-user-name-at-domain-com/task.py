757d1464b97f1e66b2af781990a9b37262439794
skoulouzis <skoulouzis@users.noreply.github.com>
2024-04-23 11:40:06 +0200

    add1-dev-user-name-at-domain-com update

--- add1-dev-user-name-at-domain-com/task.py
+++ add1-dev-user-name-at-domain-com/task.py
@@ -1,5 +1,6 @@
 
 import argparse
+import json
 arg_parser = argparse.ArgumentParser()
 
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
@@ -20,7 +21,6 @@ count = args.count
 
 a = count + 1
 
-import json
 filename = "/tmp/a_" + id + ".json"
 file_a = open(filename, "w")
 file_a.write(json.dumps(a))
