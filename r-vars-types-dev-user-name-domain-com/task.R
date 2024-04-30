fe95dc42a3c989a1a0fe73783fd4ac6135fefe23
skoulouzis <skoulouzis@users.noreply.github.com>
2024-04-30 12:25:26 +0300

    r-vars-types-dev-user-name-domain-com update

--- r-vars-types-dev-user-name-domain-com/task.R
+++ r-vars-types-dev-user-name-domain-com/task.R
@@ -8,32 +8,28 @@ library(jsonlite)
 
 option_list = list(
 
-make_option(c("--id"), action="store", default=NA, type="character", help="my description"), 
-make_option(c("--param_float"), action="store", default=NA, type="numeric", help="my description"), 
-make_option(c("--param_int"), action="store", default=NA, type="integer", help="my description"), 
-make_option(c("--param_list_int"), action="store", default=NA, type="character", help="my description"), 
-make_option(c("--param_list_str"), action="store", default=NA, type="character", help="my description"), 
-make_option(c("--param_string"), action="store", default=NA, type="character", help="my description"), 
-make_option(c("--param_string_with_comment"), action="store", default=NA, type="character", help="my description")
+make_option(c("--id"), action="store", default=NA, type="character", help="my description")
 
 )
 
 # set input parameters accordingly
 opt = parse_args(OptionParser(option_list=option_list))
 
+id <- gsub('"', '', opt$id)
 
-conf_string = 'param_string value'
-conf_string_with_comment = 'param_string value'
-conf_int = 1
-conf_float = 1.1
-conf_list_int = [1, 2, 3]
-conf_list_str = ['list_str', 'space in elem', '3']
+
+
+
+
+
 var_string <- 'var_string value'
 var_string_with_comment <- 'var_string value'  # comment
 var_int <- 1
 var_float <- 1.1
-var_list_int <- c(1, 2, 3)
-var_list_str <- c("list_str", "space in elem", "3")
+var_list_int <- list(1, 2, 3)
+var_list_str <- list("list_str", "space in elem", "3")
+print(class(var_list_int))
+
 
 
 # capturing outputs
