aa67b7ad7e9e450a96f1cd3b48d23ab16e7a1bd9
skoulouzis <skoulouzis@users.noreply.github.com>
2024-05-06 20:20:16 +0300

    r-vars-types-dev-user-name-domain-com update

--- r-vars-types-dev-user-name-domain-com/task.R
+++ r-vars-types-dev-user-name-domain-com/task.R
@@ -18,7 +18,7 @@ opt = parse_args(OptionParser(option_list=option_list))
 id <- gsub('"', '', opt$id)
 
 
-
+print('-----------Running cell----------------')
 
 
 var_string <- 'var_string value'
@@ -28,25 +28,30 @@ var_float <- 1.1
 var_list_int <- list(1, 2, 3)
 var_list_str <- list("list_str", "space in elem", "3")
 print(class(var_list_int))
-
-
+print('-----------Cell executed----------------')
 
 # capturing outputs
+print('Serialization of var_string')
 file <- file(paste0('/tmp/var_string_', id, '.json'))
 writeLines(toJSON(var_string, auto_unbox=TRUE), file)
 close(file)
+print('Serialization of var_string_with_comment')
 file <- file(paste0('/tmp/var_string_with_comment_', id, '.json'))
 writeLines(toJSON(var_string_with_comment, auto_unbox=TRUE), file)
 close(file)
+print('Serialization of var_int')
 file <- file(paste0('/tmp/var_int_', id, '.json'))
 writeLines(toJSON(var_int, auto_unbox=TRUE), file)
 close(file)
+print('Serialization of var_float')
 file <- file(paste0('/tmp/var_float_', id, '.json'))
 writeLines(toJSON(var_float, auto_unbox=TRUE), file)
 close(file)
+print('Serialization of var_list_int')
 file <- file(paste0('/tmp/var_list_int_', id, '.json'))
 writeLines(toJSON(var_list_int, auto_unbox=TRUE), file)
 close(file)
+print('Serialization of var_list_str')
 file <- file(paste0('/tmp/var_list_str_', id, '.json'))
 writeLines(toJSON(var_list_str, auto_unbox=TRUE), file)
 close(file)
