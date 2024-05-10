e8867120d020291bb53872ff1aa40271d51d7f5c
skoulouzis <skoulouzis@users.noreply.github.com>
2024-05-10 14:03:06 +0300

    consume-climwin-dev-user-name-at-domain-com update

--- consume-climwin-dev-user-name-at-domain-com/task.R
+++ consume-climwin-dev-user-name-at-domain-com/task.R
@@ -13,6 +13,7 @@ if (!requireNamespace("zoo", quietly = TRUE)) {
 }
 library(zoo)
 
+print("Retrieving input parameters")
 
 option_list = list(
 
@@ -21,20 +22,4 @@ make_option(c("--rolling_mean_temp_str"), action="store", default=NA, type="char
 make_option(c("--temperature_data_str"), action="store", default=NA, type="character", help="my description")
 
 )
-
-# set input parameters accordingly
-opt = parse_args(OptionParser(option_list=option_list))
-
-id <- gsub('"', '', opt$id)
-rolling_mean_temp_str <- gsub('"', '', opt$rolling_mean_temp_str)
-temperature_data_str <- gsub('"', '', opt$temperature_data_str)
-
-
-
-
-
-
-cat("Original Temperature Data:\n", head(temperature_data_str), "\n\n")
-cat("Rolling Mean Temperature in Moving Windows:\n", head(coredata(rolling_mean_temp_str)), "\n")
-a = 0.8059984614822301
-
+print(option_list)
