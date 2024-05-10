d46c1e8e817f2257adf40d88cf12d8a9802733d8
skoulouzis <skoulouzis@users.noreply.github.com>
2024-05-10 14:03:37 +0300

    create-file-r-dev-user-name-at-domain-com update

--- create-file-r-dev-user-name-at-domain-com/task.R
+++ create-file-r-dev-user-name-at-domain-com/task.R
@@ -5,36 +5,11 @@ setwd('/app')
 library(optparse)
 library(jsonlite)
 
+print("Retrieving input parameters")
 
 option_list = list(
 
 make_option(c("--id"), action="store", default=NA, type="character", help="my description")
 
 )
-
-# set input parameters accordingly
-opt = parse_args(OptionParser(option_list=option_list))
-
-id <- gsub('"', '', opt$id)
-
-
-
-
-
-
-L <- c("a", "b", "c")
-
-conf_data_folder <- "/tmp/data"
-file_path <- file.path(conf_data_folder, "hello.txt")
-
-writeLines(L, file_path)
-
-onlyfiles <- list.files(conf_data_folder, full.names = TRUE)
-
-print(onlyfiles)
-a = 0.4722037666089621
-
-# capturing outputs
-file <- file(paste0('/tmp/file_path_', id, '.json'))
-writeLines(toJSON(file_path, auto_unbox=TRUE), file)
-close(file)
+print(option_list)
