3eef4a195156ba3ba36e03cca177771ad6552dea
skoulouzis <skoulouzis@users.noreply.github.com>
2024-05-13 14:53:53 +0300

    visualize-rasterio-sample-tif-dev-user-name-domain-com creation

---
+++ visualize-rasterio-sample-tif-dev-user-name-domain-com/task.py
@@ -0,0 +1,38 @@
+from matplotlib import pyplot
+import rasterio
+from rasterio.plot import show
+from rasterio.plot import show_hist
+
+import argparse
+import papermill as pm
+
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--download_done', action='store', type=str, required=True, dest='download_done')
+
+arg_parser.add_argument('--filename', action='store', type=str, required=True, dest='filename')
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+parameters = {}
+
+download_done = args.download_done.replace('"','')
+parameters['download_done'] = download_done
+filename = args.filename.replace('"','')
+parameters['filename'] = filename
+
+
+
+pm.execute_notebook(
+    'visualize-rasterio-sample-tif-dev-user-name-domain-com.ipynb',
+    'visualize-rasterio-sample-tif-dev-user-name-domain-com-output.ipynb',
+    prepare_only=True,
+    parameters=dict(parameters)
+)
+
