f2f46d5140db6b0c0c14526975e985f28dd3aa16
skoulouzis <skoulouzis@users.noreply.github.com>
2024-05-16 23:51:53 +0300

    consume-climwin-dev-user-name-at-domain-com update

--- consume-climwin-dev-user-name-at-domain-com/task.R
+++ consume-climwin-dev-user-name-at-domain-com/task.R
@@ -25,10 +25,7 @@ print("------------------Option list------------------")
 print(option_list)
 
 
-# set input parameters accordingly
 opt = parse_args(OptionParser(option_list=option_list))
-
-
 
 var_serialization <- function(var){
     if (is.null(var)){
