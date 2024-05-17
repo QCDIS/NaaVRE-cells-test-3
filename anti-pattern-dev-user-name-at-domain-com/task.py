a0e1f5407f9dda2e84455937379d0e3e19e31f2a
skoulouzis <skoulouzis@users.noreply.github.com>
2024-05-17 13:31:53 +0300

    anti-pattern-dev-user-name-at-domain-com update

--- anti-pattern-dev-user-name-at-domain-com/task.py
+++ anti-pattern-dev-user-name-at-domain-com/task.py
@@ -1,6 +1,5 @@
 
 import argparse
-import json
 arg_parser = argparse.ArgumentParser()
 
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
@@ -24,8 +23,8 @@ count = args.count
 some_list = range(count, a+1)
 
 msg = '1'
-a = 0.8411183935136501
 
+import json
 filename = "/tmp/msg_" + id + ".json"
 file_msg = open(filename, "w")
 file_msg.write(json.dumps(msg))
