894cb472f1d0e54d7c9d066c4c2b57bfb7d1a711
skoulouzis <skoulouzis@users.noreply.github.com>
2024-05-19 13:29:40 +0300

    loop-file-lines-dev-user-name-at-domain-com update

--- loop-file-lines-dev-user-name-at-domain-com/task.py
+++ loop-file-lines-dev-user-name-at-domain-com/task.py
@@ -23,7 +23,6 @@ count = 0
 for l in lines:
     count += 1
     print("Line{}: {}".format(count, l.strip()))
-a = 0.2540039842276729
 
 filename = "/tmp/count_" + id + ".json"
 file_count = open(filename, "w")
