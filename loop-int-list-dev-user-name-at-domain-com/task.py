dd6949d4bc2c46d2b28258de43502e1d89d4743e
skoulouzis <skoulouzis@users.noreply.github.com>
2024-05-20 23:10:39 +0300

    loop-int-list-dev-user-name-at-domain-com update

--- loop-int-list-dev-user-name-at-domain-com/task.py
+++ loop-int-list-dev-user-name-at-domain-com/task.py
@@ -22,6 +22,7 @@ list_of_ints = json.loads(args.list_of_ints)
 for i in list_of_ints:
     a = i -1
     print(a)
+a = 0.6881432627681048
 
 file_a = open("/tmp/a_" + id + ".json", "w")
 file_a.write(json.dumps(a))
