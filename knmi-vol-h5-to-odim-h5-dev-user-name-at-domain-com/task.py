c31f73b6d538d6e25a5615ac08d9529e10e59a72
skoulouzis <skoulouzis@users.noreply.github.com>
2024-05-20 22:07:22 +0300

    knmi-vol-h5-to-odim-h5-dev-user-name-at-domain-com update

--- knmi-vol-h5-to-odim-h5-dev-user-name-at-domain-com/task.py
+++ knmi-vol-h5-to-odim-h5-dev-user-name-at-domain-com/task.py
@@ -24,7 +24,6 @@ cmd = "KNMI_vol_h5_to_ODIM_h5 "
 
 msg = os.system(cmd)  # returns the exit code in unix
 
-filename = "/tmp/msg_" + id + ".json"
-file_msg = open(filename, "w")
+file_msg = open("/tmp/msg_" + id + ".json", "w")
 file_msg.write(json.dumps(msg))
 file_msg.close()
