91392138a2555fcbce8b0ba3e6a3e043006c026d
skoulouzis <skoulouzis@users.noreply.github.com>
2024-06-07 13:58:26 +0300

    download-sample-tif-user creation

---
+++ download-sample-tif-user/task.py
@@ -0,0 +1,39 @@
+import requests
+
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+
+
+
+
+
+
+url = "https://github.com/mommermi/geotiff_sample/raw/master/sample.tif"
+filename = "/tmp/data/sample.tif"
+
+response = requests.get(url)
+
+
+
+if response.status_code == 200:
+    with open(filename, 'wb') as f:
+        f.write(response.content)
+    print("File downloaded successfully.")
+else:
+    print("Failed to download the file. Status code:", response.status_code)
+
+import json
+filename = "/tmp/filename_" + id + ".json"
+file_filename = open(filename, "w")
+file_filename.write(json.dumps(filename))
+file_filename.close()
