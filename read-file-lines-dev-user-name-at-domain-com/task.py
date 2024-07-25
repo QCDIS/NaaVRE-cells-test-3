dc2bef64eaee96afc0528d10246311a4ddabf536
skoulouzis <skoulouzis@users.noreply.github.com>
2024-07-25 11:57:07 +0300

    read-file-lines-dev-user-name-at-domain-com update

--- read-file-lines-dev-user-name-at-domain-com/task.py
+++ read-file-lines-dev-user-name-at-domain-com/task.py
@@ -35,7 +35,6 @@ print(onlyfiles)
 f = open(file_path, 'r')
 lines = f.readlines()
 f.close()
-a = 0.9334045588493926
 
 file_lines = open("/tmp/lines_" + id + ".json", "w")
 file_lines.write(json.dumps(lines))
