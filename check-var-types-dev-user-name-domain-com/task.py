a0a57ec5e84f4a6cd411c520c73807dd3a29134c
skoulouzis <skoulouzis@users.noreply.github.com>
2024-07-28 19:26:45 +0300

    check-var-types-dev-user-name-domain-com update

--- check-var-types-dev-user-name-domain-com/task.py
+++ check-var-types-dev-user-name-domain-com/task.py
@@ -207,6 +207,7 @@ for i in var_list_str:
 print('All vars are of the correct type')
 
 done = 'True'
+a = 0.2383348452193047
 
 file_done = open("/tmp/done_" + id + ".json", "w")
 file_done.write(json.dumps(done))
