f608c32b5d15949527ce73f87a8c19244649d45e
skoulouzis <skoulouzis@users.noreply.github.com>
2024-08-29 13:22:27 +0300

    process-a-list-dev-user-name-domain-com creation

---
+++ process-a-list-dev-user-name-domain-com/task.py
@@ -0,0 +1,30 @@
+
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--my_list', action='store', type=str, required=True, dest='my_list')
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+my_list = json.loads(args.my_list)
+
+
+
+b_list = []
+for elem in my_list:
+    
+    b_list.append(elem * 2)
+    
+    
+    
+
