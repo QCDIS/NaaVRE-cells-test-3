f7f6bc964ddeace354802072213c2100b1da7c94
skoulouzis <skoulouzis@users.noreply.github.com>
2024-09-11 11:04:21 +0300

    process-cell-dev-user-name-domain-com creation

---
+++ process-cell-dev-user-name-domain-com/task.R
@@ -0,0 +1,70 @@
+setwd('/app')
+library(optparse)
+library(jsonlite)
+
+
+
+print('option_list')
+option_list = list(
+
+make_option(c("--id"), action="store", default=NA, type="character", help="my description"), 
+make_option(c("--var_1"), action="store", default=NA, type="character", help="my description")
+)
+
+
+opt = parse_args(OptionParser(option_list=option_list))
+
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
+
+print("Retrieving id")
+var = opt$id
+print(var)
+var_len = length(var)
+print(paste("Variable id has length", var_len))
+
+id <- gsub("\"", "", opt$id)
+print("Retrieving var_1")
+var = opt$var_1
+print(var)
+var_len = length(var)
+print(paste("Variable var_1 has length", var_len))
+
+var_1 <- gsub("\"", "", opt$var_1)
+
+
+print("Running the cell")
+var_2 = paste(var_1,'world')
+
+print(var_2)
+# capturing outputs
+print('Serialization of var_2')
+file <- file(paste0('/tmp/var_2_', id, '.json'))
+writeLines(toJSON(var_2, auto_unbox=TRUE), file)
+close(file)
