fd7ba7429eb70913f80d889ef97effdd47e86e60
skoulouzis <skoulouzis@users.noreply.github.com>
2024-10-10 17:06:32 +0200

    create-file-dev-user-name-at-domain-com update

--- create-file-dev-user-name-at-domain-com/task.py
+++ create-file-dev-user-name-at-domain-com/task.py
@@ -1,28 +1,55 @@
+setwd('/app')
+library(optparse)
+library(jsonlite)
+
 from os.path import isfile
 from os.path import join
 from os import listdir
 import os
 
-import argparse
-import json
-import os
-arg_parser = argparse.ArgumentParser()
 
 
-arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+print('option_list')
+option_list = list(
+
+)
 
 
+opt = parse_args(OptionParser(option_list=option_list))
 
-args = arg_parser.parse_args()
-print(args)
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
 
-id = args.id
 
+{'conf_data_folder': "os.path.join('/tmp','data')"}
 
-
-conf_data_folder = os.path.join('/tmp','data')
-
-
+print("Running the cell")
 conf_data_folder = os.path.join('/tmp','data')
 
 L = ["a\n", "b\n", "c\n"]
@@ -34,7 +61,8 @@ fp.close()
 onlyfiles = [f for f in listdir(conf_data_folder) if isfile(join(conf_data_folder, f))]
 
 print(onlyfiles)
-
-file_file_path = open("/tmp/file_path_" + id + ".json", "w")
-file_file_path.write(json.dumps(file_path))
-file_file_path.close()
+# capturing outputs
+print('Serialization of file_path')
+file <- file(paste0('/tmp/file_path_', id, '.json'))
+writeLines(toJSON(file_path, auto_unbox=TRUE), file)
+close(file)
