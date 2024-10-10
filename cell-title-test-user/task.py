c2515ac0a345e296abde87e559d2ce862bd19cd1
skoulouzis <skoulouzis@users.noreply.github.com>
2024-10-10 17:04:09 +0200

    cell-title-test-user update

--- cell-title-test-user/task.py
+++ cell-title-test-user/task.py
@@ -1,45 +1,57 @@
+setwd('/app')
+library(optparse)
+library(jsonlite)
+
 import math as m
 from math import nan
 
-import argparse
-import json
-import os
-arg_parser = argparse.ArgumentParser()
 
-secret_test = os.getenv('secret_test')
+secret_test = Sys.getenv('secret_test')
 
-arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+print('option_list')
+option_list = list(
 
-
-arg_parser.add_argument('--a', action='store', type=int, required=True, dest='a')
-
-arg_parser.add_argument('--b', action='store', type=float, required=True, dest='b')
-
-arg_parser.add_argument('--c', action='store', type=str, required=True, dest='c')
-
-arg_parser.add_argument('--d', action='store', type=str, required=True, dest='d')
-
-arg_parser.add_argument('--e', action='store', type=float, required=True, dest='e')
-
-arg_parser.add_argument('--param_test', action='store', type=str, required=True, dest='param_test')
-
-args = arg_parser.parse_args()
-print(args)
-
-id = args.id
-
-a = args.a
-b = args.b
-c = args.c.replace('"','')
-d = json.loads(args.d)
-e = args.e
-
-param_test = args.param_test.replace('"','')
+)
 
 
+opt = parse_args(OptionParser(option_list=option_list))
+
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
+
+
+
+print("Running the cell")
 print(param_test, secret_test, a, b, c, d, e)
 f = [m.pi, nan]
-
-file_f = open("/tmp/f_" + id + ".json", "w")
-file_f.write(json.dumps(f))
-file_f.close()
+# capturing outputs
+print('Serialization of f')
+file <- file(paste0('/tmp/f_', id, '.json'))
+writeLines(toJSON(f, auto_unbox=TRUE), file)
+close(file)
