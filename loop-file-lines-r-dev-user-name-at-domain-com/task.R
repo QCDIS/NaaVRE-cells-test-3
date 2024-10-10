e64f53e74c5f49037c95baf01e25943d359ad4d4
skoulouzis <skoulouzis@users.noreply.github.com>
2024-10-10 17:14:04 +0200

    loop-file-lines-r-dev-user-name-at-domain-com update

--- loop-file-lines-r-dev-user-name-at-domain-com/task.R
+++ loop-file-lines-r-dev-user-name-at-domain-com/task.R
@@ -4,11 +4,10 @@ library(jsonlite)
 
 
 
+
 print('option_list')
 option_list = list(
 
-make_option(c("--id"), action="store", default=NA, type="character", help="my description"), 
-make_option(c("--lines"), action="store", default=NA, type="character", help="my description")
 )
 
 
@@ -42,24 +41,6 @@ var_serialization <- function(var){
         }
     )
 }
-
-print("Retrieving id")
-var = opt$id
-print(var)
-var_len = length(var)
-print(paste("Variable id has length", var_len))
-
-id <- gsub("\"", "", opt$id)
-print("Retrieving lines")
-var = opt$lines
-print(var)
-var_len = length(var)
-print(paste("Variable lines has length", var_len))
-
-print("------------------------Running var_serialization for lines-----------------------")
-print(opt$lines)
-lines = var_serialization(opt$lines)
-print("---------------------------------------------------------------------------------")
 
 
 
