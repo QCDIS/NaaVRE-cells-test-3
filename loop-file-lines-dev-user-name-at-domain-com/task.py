8365cf7739552f546211f03fcd307fafa2429e81
skoulouzis <skoulouzis@users.noreply.github.com>
2024-10-10 16:53:31 +0200

    loop-file-lines-dev-user-name-at-domain-com update

--- loop-file-lines-dev-user-name-at-domain-com/task.py
+++ loop-file-lines-dev-user-name-at-domain-com/task.py
@@ -1,31 +1,57 @@
-
-import argparse
-import json
-import os
-arg_parser = argparse.ArgumentParser()
-
-
-arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
-
-
-arg_parser.add_argument('--lines', action='store', type=str, required=True, dest='lines')
-
-
-args = arg_parser.parse_args()
-print(args)
-
-id = args.id
-
-lines = json.loads(args.lines)
+setwd('/app')
+library(optparse)
+library(jsonlite)
 
 
 
+
+print('option_list')
+option_list = list(
+
+)
+
+
+opt = parse_args(OptionParser(option_list=option_list))
+
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
+
+
+
+print("Running the cell")
 
 count = 0
 for l in lines:
     count += 1
     print("Line{}: {}".format(count, l.strip()))
-
-file_count = open("/tmp/count_" + id + ".json", "w")
-file_count.write(json.dumps(count))
-file_count.close()
+# capturing outputs
+print('Serialization of count')
+file <- file(paste0('/tmp/count_', id, '.json'))
+writeLines(toJSON(count, auto_unbox=TRUE), file)
+close(file)
