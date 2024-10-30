08a9a1ca737e7e87782296a1289360c7dc20995a
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2024-10-30 17:05:41 +0100

    cell-title-test-user-2 creation

---
+++ cell-title-test-user-2/task.py
@@ -0,0 +1,57 @@
+setwd('/app')
+library(optparse)
+library(jsonlite)
+
+import math as m
+from math import nan
+
+
+secret_test = Sys.getenv('secret_test')
+
+print('option_list')
+option_list = list(
+
+)
+
+
+opt = parse_args(OptionParser(option_list=option_list))
+
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
+
+
+
+print("Running the cell")
+print(param_test, secret_test, a, b, c, d, e)
+f = [m.pi, nan]
+# capturing outputs
+print('Serialization of f')
+file <- file(paste0('/tmp/f_', id, '.json'))
+writeLines(toJSON(f, auto_unbox=TRUE), file)
+close(file)
