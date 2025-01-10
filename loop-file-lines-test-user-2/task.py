2a6eb1a0e16db055fbf4cf9595ca273b5832c2c2
skoulouzis <skoulouzis@users.noreply.github.com>
2025-01-10 15:39:34 +0100

    loop-file-lines-test-user-2 creation

---
+++ loop-file-lines-test-user-2/task.py
@@ -0,0 +1,56 @@
+setwd('/app')
+library(optparse)
+library(jsonlite)
+
+
+
+
+print('option_list')
+option_list = list(
+
+)
+
+
+opt = parse_args(OptionParser(option_list=option_list))
+
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
+
+
+
+print("Running the cell")
+count = 0
+for l in lines:
+    count += 1
+    print("Line{}: {}".format(count, l.strip()))
+# capturing outputs
+print('Serialization of {'name': 'count', 'type': 'int'}')
+file <- file(paste0('/tmp/{'name': 'count', 'type': 'int'}_', id, '.json'))
+writeLines(toJSON({'name': 'count', 'type': 'int'}, auto_unbox=TRUE), file)
+close(file)
