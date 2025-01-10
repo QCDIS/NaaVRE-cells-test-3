aa4cfb0ce150b0889bb7d0940c21e31032bdcc19
skoulouzis <skoulouzis@users.noreply.github.com>
2025-01-10 15:20:01 +0100

    secret-reader-test-user-2 creation

---
+++ secret-reader-test-user-2/task.py
@@ -0,0 +1,49 @@
+setwd('/app')
+library(optparse)
+library(jsonlite)
+
+
+
+{'name': 'secret_xyz', 'type': 'str'} = Sys.getenv('{'name': 'secret_xyz', 'type': 'str'}')
+
+print('option_list')
+option_list = list(
+
+)
+
+
+opt = parse_args(OptionParser(option_list=option_list))
+
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
+
+
+
+print("Running the cell")
+print(param_abc, secret_xyz, foo)
