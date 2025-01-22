a7a13f96badbdd0613c7371285847136bfaa71a4
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2025-01-22 14:32:34 +0100

    untitled-test-user-2 update

--- untitled-test-user-2/task.py
+++ untitled-test-user-2/task.py
@@ -45,9 +45,4 @@ var_serialization <- function(var){
 
 
 print("Running the cell")
-a = 1
-# capturing outputs
-print('Serialization of {'name': 'a', 'type': 'int'}')
-file <- file(paste0('/tmp/{'name': 'a', 'type': 'int'}_', id, '.json'))
-writeLines(toJSON({'name': 'a', 'type': 'int'}, auto_unbox=TRUE), file)
-close(file)
+print(a)
