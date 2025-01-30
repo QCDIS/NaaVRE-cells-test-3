d542a5cadea163d2319311c6e08b9fe666067ed8
skoulouzis <skoulouzis@users.noreply.github.com>
2025-01-30 21:07:51 +0100

    create-file-test-user-2 update

--- create-file-test-user-2/task.py
+++ create-file-test-user-2/task.py
@@ -1,55 +1,27 @@
-setwd('/app')
-library(optparse)
-library(jsonlite)
+from os.path import isfile
+from os.path import join
+from os import listdir
+import os
 
-{'asname': None, 'module': 'os.path', 'name': 'isfile'}
-{'asname': None, 'module': 'os.path', 'name': 'join'}
-{'asname': None, 'module': 'os', 'name': 'listdir'}
-{'asname': None, 'module': '', 'name': 'os'}
-
-
-
-print('option_list')
-option_list = list(
-
-)
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
 
 
-opt = parse_args(OptionParser(option_list=option_list))
-
-var_serialization <- function(var){
-    if (is.null(var)){
-        print("Variable is null")
-        exit(1)
-    }
-    tryCatch(
-        {
-            var <- fromJSON(var)
-            print("Variable deserialized")
-            return(var)
-        },
-        error=function(e) {
-            print("Error while deserializing the variable")
-            print(var)
-            var <- gsub("'", '"', var)
-            var <- fromJSON(var)
-            print("Variable deserialized")
-            return(var)
-        },
-        warning=function(w) {
-            print("Warning while deserializing the variable")
-            var <- gsub("'", '"', var)
-            var <- fromJSON(var)
-            print("Variable deserialized")
-            return(var)
-        }
-    )
-}
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
-{'name': 'conf_data_folder', 'assignation': "conf_data_folder = os.path.join('/tmp', 'data')"}
 
-print("Running the cell")
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+
+
+conf_data_folder = conf_data_folder = os.path.join('/tmp', 'data')
+
 L = ["a\n", "b\n", "c\n"]
 file_path =  os.path.join(conf_data_folder,'hello.txt')
 fp = open(file_path, 'w')
@@ -59,8 +31,7 @@ fp.close()
 onlyfiles = [f for f in listdir(conf_data_folder) if isfile(join(conf_data_folder, f))]
 
 print(onlyfiles)
-# capturing outputs
-print('Serialization of {'name': 'file_path', 'type': 'str'}')
-file <- file(paste0('/tmp/{'name': 'file_path', 'type': 'str'}_', id, '.json'))
-writeLines(toJSON({'name': 'file_path', 'type': 'str'}, auto_unbox=TRUE), file)
-close(file)
+
+file_file_path = open("/tmp/file_path_" + id + ".json", "w")
+file_file_path.write(json.dumps(file_path))
+file_file_path.close()
