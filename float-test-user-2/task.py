6228ae809cd94393cf5ab5388c1896e64f53cbbc
skoulouzis <skoulouzis@users.noreply.github.com>
2025-01-30 21:28:06 +0100

    float-test-user-2 update

--- float-test-user-2/task.py
+++ float-test-user-2/task.py
@@ -1,53 +1,24 @@
-setwd('/app')
-library(optparse)
-library(jsonlite)
+
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
 
 
 
 
-print('option_list')
-option_list = list(
-
-)
-
-
-opt = parse_args(OptionParser(option_list=option_list))
-
-var_serialization <- function(var){
-    if (is.null(var)){
-        print("Variable is null")
-        exit(1)
-    }
-    tryCatch(
-        {
-            var <- fromJSON(var)
-            print("Variable deserialized")
-            return(var)
-        },
-        error=function(e) {
-            print("Error while deserializing the variable")
-            print(var)
-            var <- gsub("'", '"', var)
-            var <- fromJSON(var)
-            print("Variable deserialized")
-            return(var)
-        },
-        warning=function(w) {
-            print("Warning while deserializing the variable")
-            var <- gsub("'", '"', var)
-            var <- fromJSON(var)
-            print("Variable deserialized")
-            return(var)
-        }
-    )
-}
-
-
-
-print("Running the cell")
 c = 2.4
-# capturing outputs
-print('Serialization of {'name': 'c', 'type': 'float'}')
-file <- file(paste0('/tmp/{'name': 'c', 'type': 'float'}_', id, '.json'))
-writeLines(toJSON({'name': 'c', 'type': 'float'}, auto_unbox=TRUE), file)
-close(file)
+
+file_c = open("/tmp/c_" + id + ".json", "w")
+file_c.write(json.dumps(c))
+file_c.close()
