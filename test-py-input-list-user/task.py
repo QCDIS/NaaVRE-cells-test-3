3997ae5242881956aa33470b34b54ee33317230f
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2025-02-20 21:46:38 +0100

    test-py-input-list-user update

--- test-py-input-list-user/task.py
+++ test-py-input-list-user/task.py
@@ -1,53 +1,24 @@
-setwd('/app')
-library(optparse)
-library(jsonlite)
+
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
 
 
 
 
-print('option_list')
-option_list = list(
-
-)
-
-
-opt = parse_args(OptionParser(option_list=option_list))
-
-var_serialization <- function(var){
-    if (is.null(var)){
-        print("Variable is null")
-        exit(1)
-    }
-    tryCatch(
-        {
-            var <- fromJSON(var)
-            print("Variable deserialized")
-            return(var)
-        },
-        error=function(e) {
-            print("Error while deserializing the variable")
-            print(var)
-            var <- gsub("'", '"', var)
-            var <- fromJSON(var)
-            print("Variable deserialized")
-            return(var)
-        },
-        warning=function(w) {
-            print("Warning while deserializing the variable")
-            var <- gsub("'", '"', var)
-            var <- fromJSON(var)
-            print("Variable deserialized")
-            return(var)
-        }
-    )
-}
-
-
-
-print("Running the cell")
 names = ["Alice", "Bob"]
-# capturing outputs
-print('Serialization of {'name': 'names', 'type': 'list'}')
-file <- file(paste0('/tmp/{'name': 'names', 'type': 'list'}_', id, '.json'))
-writeLines(toJSON({'name': 'names', 'type': 'list'}, auto_unbox=TRUE), file)
-close(file)
+
+file_names = open("/tmp/names_" + id + ".json", "w")
+file_names.write(json.dumps(names))
+file_names.close()
