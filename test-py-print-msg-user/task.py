2ec1897876725109d7b54a07a5f91ea97983b626
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2025-03-13 14:37:13 +0100

    test-py-print-msg-user creation

---
+++ test-py-print-msg-user/task.py
@@ -0,0 +1,24 @@
+
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--msg', action='store', type=str, required=True, dest='msg')
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+msg = args.msg.replace('"','')
+
+
+
+print(msg)
+
