07c0aabac1d601042668a2adee7a084e73e476b2
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2025-03-13 16:49:17 +0100

    py-types-set-user creation

---
+++ py-types-set-user/task.py
@@ -0,0 +1,44 @@
+
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+
+
+
+var_string = 'var_string value'
+var_string_with_comment = 'var_string value'  # comment
+var_int = 1
+var_float = 1.1
+var_list_int = [1, 2, 3]
+var_list_str = ["list_str", "space in elem", "3"]
+
+file_var_float = open("/tmp/var_float_" + id + ".json", "w")
+file_var_float.write(json.dumps(var_float))
+file_var_float.close()
+file_var_int = open("/tmp/var_int_" + id + ".json", "w")
+file_var_int.write(json.dumps(var_int))
+file_var_int.close()
+file_var_list_int = open("/tmp/var_list_int_" + id + ".json", "w")
+file_var_list_int.write(json.dumps(var_list_int))
+file_var_list_int.close()
+file_var_list_str = open("/tmp/var_list_str_" + id + ".json", "w")
+file_var_list_str.write(json.dumps(var_list_str))
+file_var_list_str.close()
+file_var_string = open("/tmp/var_string_" + id + ".json", "w")
+file_var_string.write(json.dumps(var_string))
+file_var_string.close()
+file_var_string_with_comment = open("/tmp/var_string_with_comment_" + id + ".json", "w")
+file_var_string_with_comment.write(json.dumps(var_string_with_comment))
+file_var_string_with_comment.close()
