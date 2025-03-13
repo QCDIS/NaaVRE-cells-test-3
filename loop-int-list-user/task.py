5687632ae4efa54c555160c17adfcb33c189753a
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2025-03-13 13:16:07 +0100

    loop-int-list-user creation

---
+++ loop-int-list-user/task.py
@@ -0,0 +1,29 @@
+
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--list_of_ints', action='store', type=str, required=True, dest='list_of_ints')
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+list_of_ints = json.loads(args.list_of_ints)
+
+
+
+for i in list_of_ints:
+    a = i -1
+    print(a)
+
+file_a = open("/tmp/a_" + id + ".json", "w")
+file_a.write(json.dumps(a))
+file_a.close()
