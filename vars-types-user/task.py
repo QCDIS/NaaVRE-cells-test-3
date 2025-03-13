468bc7de33fdafe9638c3a6b9c84fea3a5f474eb
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2025-03-13 16:42:25 +0100

    vars-types-user creation

---
+++ vars-types-user/task.py
@@ -0,0 +1,65 @@
+
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+
+
+
+conf_string = 'conf_string value'
+conf_string_with_comment = 'conf_string value'  # comment
+conf_int = 1
+conf_float = 1.1
+conf_list_int = [1, 2, 3]
+conf_list_str = ["list_str", "space in elem", "3"]
+
+param_string = 'param_string value'
+param_string_with_comment = 'param_string value'  # comment
+param_int = 1
+param_float = 1.1
+param_list_int = [1, 2, 3]
+param_list_str = ["list_str", "space in elem", "3"]
+
+secret_string = 'secret_string value'
+secret_string_with_comment = 'secret_string value'  # comment
+secret_int = 1
+secret_float = 1.1
+secret_list_int = [1, 2, 3]
+secret_list_str = ["list_str", "space in elem", "3"]
+
+var_string = 'var_string value'
+var_string_with_comment = 'var_string value'  # comment
+var_int = 1
+var_float = 1.1
+var_list_int = [1, 2, 3]
+var_list_str = ["list_str", "space in elem", "3"]
+
+file_var_float = open("/tmp/var_float_" + id + ".json", "w")
+file_var_float.write(json.dumps(var_float))
+file_var_float.close()
+file_var_int = open("/tmp/var_int_" + id + ".json", "w")
+file_var_int.write(json.dumps(var_int))
+file_var_int.close()
+file_var_list_int = open("/tmp/var_list_int_" + id + ".json", "w")
+file_var_list_int.write(json.dumps(var_list_int))
+file_var_list_int.close()
+file_var_list_str = open("/tmp/var_list_str_" + id + ".json", "w")
+file_var_list_str.write(json.dumps(var_list_str))
+file_var_list_str.close()
+file_var_string = open("/tmp/var_string_" + id + ".json", "w")
+file_var_string.write(json.dumps(var_string))
+file_var_string.close()
+file_var_string_with_comment = open("/tmp/var_string_with_comment_" + id + ".json", "w")
+file_var_string_with_comment.write(json.dumps(var_string_with_comment))
+file_var_string_with_comment.close()
