49c6f7e8882272aa865da9ecec423524cbee1da7
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2025-03-13 17:22:27 +0100

    print-param-user update

--- print-param-user/task.py
+++ print-param-user/task.py
@@ -8,7 +8,7 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
-arg_parser.add_argument('--param_list', action='store', type=str, required=True, dest='param_list')
+arg_parser.add_argument('--param_int', action='store', type=int, required=True, dest='param_int')
 
 args = arg_parser.parse_args()
 print(args)
@@ -16,17 +16,8 @@ print(args)
 id = args.id
 
 
-print(args.param_list)
-print(type(args.param_list))
-try:
-    param_list = json.loads(args.param_list)
-except Exception as e:
-    if e.__class__.__name__ == 'JSONDecodeError':
-        import ast
-        param_list = ast.literal_eval(args.param_list.replace('[','["').replace(',','","').replace('" ','"').replace(']','"]').replace("'",""))
-    else:
-        raise e
+param_int = args.param_int
 
 
-print(param_list)
+print(param_int)
 
