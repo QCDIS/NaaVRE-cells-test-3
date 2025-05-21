e12745b7940a0a448194ad661068175e1895caa3
skoulouzis <skoulouzis@users.noreply.github.com>
2025-05-21 19:01:54 +0300

    test_containerize1429f5b4-5be3-4ab2-b1c0-654ffd6dfb41

---
+++ test_containerize1429f5b4-5be3-4ab2-b1c0-654ffd6dfb41/task.py
@@ -0,0 +1,30 @@
+
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--lines', action='store', type=str, required=True, dest='lines')
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+lines = json.loads(args.lines)
+
+
+
+count = 0
+for l in lines:
+    count += 1
+    print("Line{}: {}".format(count, l.strip()))
+
+file_count = open("/tmp/count_" + id + ".json", "w")
+file_count.write(json.dumps(count))
+file_count.close()
