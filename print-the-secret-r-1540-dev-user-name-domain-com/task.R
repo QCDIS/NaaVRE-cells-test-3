4f4bfb90c008de117b706b5437b682fe3d3644c2
KoenGreuell <44636560+KoenGreuell@users.noreply.github.com>
2025-05-26 15:40:33 +0200

    print-the-secret-r-1540-dev-user-name-domain-com creation

---
+++ print-the-secret-r-1540-dev-user-name-domain-com/task.R
@@ -0,0 +1,60 @@
+setwd('/app')
+library(optparse)
+library(jsonlite)
+
+if (!requireNamespace("SecretsProvider", quietly = TRUE)) {
+	install.packages("SecretsProvider", repos="http://cran.us.r-project.org")
+}
+library(SecretsProvider)
+
+secret_data_source_example1_name_API_key = Sys.getenv('secret_data_source_example1_name_API_key')
+
+print('option_list')
+option_list = list(
+
+make_option(c("--id"), action="store", default=NA, type="character", help="my description")
+)
+
+
+opt = parse_args(OptionParser(option_list=option_list))
+
+var_serialization <- function(var){
+    if (is.null(var)){
+        print("Variable is null")
+        exit(1)
+    }
+    tryCatch(
+        {
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        error=function(e) {
+            print("Error while deserializing the variable")
+            print(var)
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        },
+        warning=function(w) {
+            print("Warning while deserializing the variable")
+            var <- gsub("'", '"', var)
+            var <- fromJSON(var)
+            print("Variable deserialized")
+            return(var)
+        }
+    )
+}
+
+print("Retrieving id")
+var = opt$id
+print(var)
+var_len = length(var)
+print(paste("Variable id has length", var_len))
+
+id <- gsub("\"", "", opt$id)
+
+
+print("Running the cell")
+print(secret_data_source_example1_name_API_key)
