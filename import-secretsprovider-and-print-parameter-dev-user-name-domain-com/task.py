d821729fa447e135b36f722ff027dfc51f0e40b1
KoenGreuell <44636560+KoenGreuell@users.noreply.github.com>
2025-05-28 14:45:08 +0200

    import-secretsprovider-and-print-parameter-dev-user-name-domain-com creation

---
+++ import-secretsprovider-and-print-parameter-dev-user-name-domain-com/task.py
@@ -0,0 +1,23 @@
+
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--param_number', action='store', type=int, required=True, dest='param_number')
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+
+param_number = args.param_number
+
+
+print(param_number)
+
