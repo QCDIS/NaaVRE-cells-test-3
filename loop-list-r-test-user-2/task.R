ffbec9f905ed199ccfad1c351215d923e17068b7
skoulouzis <skoulouzis@users.noreply.github.com>
2025-07-19 15:25:01 +0200

    loop-list-r-test-user-2

--- loop-list-r-test-user-2/task.R
+++ loop-list-r-test-user-2/task.R
@@ -57,7 +57,7 @@ print("-------------------------------------------------------------------------
 
 id <- gsub('"', '', opt$id)
 
-{'assignation': "conf_data_folder<-'/tmp/data'", 'name': 'conf_data_folder'}
+conf_data_folder<-'/tmp/data'
 
 print("Running the cell")
 for (l in list_of_paths) {
