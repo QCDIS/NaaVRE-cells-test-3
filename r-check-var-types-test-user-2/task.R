659d5e9dba2963aa07df65e99cd09c06135ff331
skoulouzis <skoulouzis@users.noreply.github.com>
2025-08-04 15:05:20 +0300

    r-check-var-types-test-user-2

--- r-check-var-types-test-user-2/task.R
+++ r-check-var-types-test-user-2/task.R
@@ -8,6 +8,12 @@ library(jsonlite)
 print('option_list')
 option_list = list(
 
+make_option(c("--param_float"), action="store", default=NA, type="numeric", help="my description"),
+make_option(c("--param_int"), action="store", default=NA, type="integer", help="my description"),
+make_option(c("--param_list_int"), action="store", default=NA, type="character", help="my description"),
+make_option(c("--param_list_str"), action="store", default=NA, type="character", help="my description"),
+make_option(c("--param_string"), action="store", default=NA, type="character", help="my description"),
+make_option(c("--param_string_with_comment"), action="store", default=NA, type="character", help="my description"),
 make_option(c("--var_float"), action="store", default=NA, type="numeric", help="my description"),
 make_option(c("--var_int"), action="store", default=NA, type="integer", help="my description"),
 make_option(c("--var_list_int"), action="store", default=NA, type="character", help="my description"),
