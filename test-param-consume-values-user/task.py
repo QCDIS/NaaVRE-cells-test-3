15c849010d367de8151e99b1959a55ca22029338
Gabriel Pelouze <9514702+gpelouze@users.noreply.github.com>
2026-04-20 15:44:23 +0200

    test-param-consume-values-user

---
+++ test-param-consume-values-user/task.py
@@ -0,0 +1,27 @@
+
+import argparse
+import json
+import os
+arg_parser = argparse.ArgumentParser()
+
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+
+arg_parser.add_argument('--a', action='store', type=str, required=True, dest='a')
+
+arg_parser.add_argument('--b', action='store', type=str, required=True, dest='b')
+
+
+args = arg_parser.parse_args()
+print(args)
+
+id = args.id
+
+a = args.a.replace('"','')
+b = args.b.replace('"','')
+
+
+
+print(a, b)
+
